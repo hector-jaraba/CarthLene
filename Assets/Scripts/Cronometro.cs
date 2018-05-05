13e8af486b5bd2dda6f400e3288b7aba3a4005c6
hjaraba <hjaraba.secpro@gmail.com>
2018-05-05 13:03:35 +0200

    Infinite Background

---
+++ Assets/Scripts/Cronometro.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cronometro : MonoBehaviour {
+
+    public GameObject motorCarreterasGo;
+    public MotorCarreteras motorCarreterasScript;
+
+    public float tiempo;
+    public float distancia;
+    public Text txtTiempo;
+    public Text txtDistancia;
+
+	// Use this for initialization
+	void Start () {
+        motorCarreterasGo = GameObject.Find("MotorCarreteras");
+        motorCarreterasScript = motorCarreterasGo.GetComponent<MotorCarreteras>();
+
+        txtTiempo.text = "2:00";
+        txtDistancia.text = "0";
+        tiempo = 120;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (motorCarreterasScript.inicioJuego && !motorCarreterasScript.juegoTerminado) {
+            CalculoTiempoDistancia();
+        }
+
+        if (tiempo <= 0 && !motorCarreterasScript.juegoTerminado) {
+            motorCarreterasScript.juegoTerminado = true;
+        }
+
+    }
+
+    void CalculoTiempoDistancia()
+    {
+        distancia += Time.deltaTime * motorCarreterasScript.velocidad;
+        txtDistancia.text = ((int)distancia).ToString();
+
+        tiempo -= Time.deltaTime;
+        int minutos = (int)tiempo / 60;
+        int segundos = (int)tiempo % 60;
+        txtTiempo.text = minutos.ToString() + ":" + segundos.ToString().PadLeft(2, '0');
+    }
+}
---
+++ Assets/Scripts/MotorCarreteras.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotorCarreteras : MonoBehaviour {
+
+    public GameObject contenedorCallesGO;
+    public GameObject[] contenedorCallesArray;
+
+    public GameObject calleNueva;
+    public GameObject calleAnterior;
+
+    public float velocidad;
+    public float tamanyoCalle;
+    public bool inicioJuego;
+    public bool juegoTerminado;
+
+    int contadorCalles = 0;
+    int numeroSelectorCalles;
+
+    public Vector3 medidaLimitePantalla;
+    public bool salioDePantalla;
+
+    public GameObject mCamGO;
+    public Camera mCamComp;
+
+	// Use this for initialization
+	void Start () {
+        InicioJuego();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (inicioJuego  && !juegoTerminado) {
+            transform.Translate(Vector3.down * velocidad * Time.deltaTime);
+
+            if (calleAnterior.transform.position.y + tamanyoCalle < medidaLimitePantalla.y && !salioDePantalla)
+            {
+                salioDePantalla = true;
+                DestruyoCalles();
+
+            }
+        }  
+	}
+
+    void InicioJuego() {
+        contenedorCallesGO = GameObject.Find("ContenedorCalles");
+        mCamGO = GameObject.Find("MainCamera");
+        mCamComp = mCamGO.GetComponent<Camera>();
+        VelocidadMotorCarretera();
+        MedirPantalla();
+        BuscoCalles();
+
+
+    }
+
+    void VelocidadMotorCarretera() {
+        velocidad = 18;
+    }
+
+    void BuscoCalles() {
+        contenedorCallesArray = GameObject.FindGameObjectsWithTag("Calle");
+
+        for (int i = 0; i < contenedorCallesArray.Length; i++) {
+            contenedorCallesArray[i].gameObject.transform.parent = contenedorCallesGO.transform;
+            contenedorCallesArray[i].gameObject.SetActive(false);
+            contenedorCallesArray[i].gameObject.name = "CalleOFF_" + i;
+        }
+
+        CrearCalles();
+    }
+
+    void CrearCalles() {
+
+        contadorCalles++;
+        numeroSelectorCalles = Random.Range(0, contenedorCallesArray.Length);
+        GameObject Calle = Instantiate(contenedorCallesArray[numeroSelectorCalles]);
+        Calle.SetActive(true);
+        Calle.name = "Calle"+contadorCalles;
+        Calle.transform.parent = gameObject.transform;
+        PosicionoCalles();
+        Debug.Log("Crear");
+
+    }
+
+    void PosicionoCalles() {
+        calleAnterior = GameObject.Find("Calle"+(contadorCalles-1));
+        calleNueva = GameObject.Find("Calle"+contadorCalles);
+        MidoCalle();
+        calleNueva.transform.position = new Vector3(calleAnterior.transform.position.x,calleAnterior.transform.position.y+tamanyoCalle, 0);
+        salioDePantalla = false;
+        Debug.Log("Posicionar");
+
+
+    }
+
+    void MidoCalle() {
+        for (int i = 0; i < calleAnterior.transform.childCount; i++) {
+
+            if (calleAnterior.transform.GetChild(i).gameObject.GetComponent<Pieza>() != null) {
+                float tamanyoPieza = calleAnterior.transform.GetChild(i).gameObject.GetComponent<SpriteRenderer>().bounds.size.y;
+                tamanyoCalle += tamanyoPieza;
+
+            }
+            
+        }
+
+    }
+
+    void MedirPantalla() {
+
+        medidaLimitePantalla = new Vector3(0, mCamComp.ScreenToWorldPoint(new Vector3(0, 0, 0)).y - 0.5f, 0);
+
+    }
+
+    void DestruyoCalles() {
+        Destroy(calleAnterior);
+        tamanyoCalle = 0;
+        calleAnterior = null;
+        CrearCalles();
+
+    }
+}
